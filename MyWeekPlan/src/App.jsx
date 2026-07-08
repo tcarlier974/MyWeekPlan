2f584ead7a0baf118a38b772823cf4a0f1091457
Tristan Carlier <carliertiti97459@gmail.com>
2026-07-08 15:52:27 +0200

    feat: add useEffect import to App component for enhanced functionality

--- MyWeekPlan/src/App.jsx
+++ MyWeekPlan/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect} from 'react'
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/react'
 import { generateWeeklyMenu, getAlternativeMeal } from './utils/solver'
