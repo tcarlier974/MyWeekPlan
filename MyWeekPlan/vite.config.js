0520cecb4691bdfbc4cfc41ed6927ca201f0c5e2
Tristan Carlier <carliertiti97459@gmail.com>
2026-07-08 06:35:55 +0200

    feat: remove @vercel/analytics dependency from package configuration

--- MyWeekPlan/vite.config.js
+++ MyWeekPlan/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { Analytics } from "@vercel/analytics/next"
 
 // https://vite.dev/config/
 export default defineConfig({
