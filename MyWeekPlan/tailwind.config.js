97442ac10440e3117bd302137d4cfc8630004c99
Tristan Carlier <carliertiti97459@gmail.com>
2026-08-13 12:54:27 +0200

    plein d'amelioration
    Tailwindcss,skills...

---
+++ MyWeekPlan/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        linen: '#F8F7F4',
+        jet: '#333333',
+        sage: '#8A9A5B',
+        'soft-yellow': '#FAD02C',
+      },
+      fontFamily: {
+        serif: ['"Playfair Display"', 'serif'],
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
